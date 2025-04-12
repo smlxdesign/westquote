6fd3abe541653188c8006b7847de9235cd65c017
Samuel <smlxdesign@duck.com>
2025-04-12 23:06:44 +0200

    Add some styling + photo of kanye west

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react';
 import Loading from './loading';
 import * as z from 'zod';
 
+import kanyeImage from './assets/kanye-image.jpg';
+
 const Quote = z.object({
 	quote: z.string(),
 });
@@ -18,17 +20,26 @@ async function loadQuote() {
 		});
 }
 
-const quote = await loadQuote();
+const quote = loadQuote();
 
 export default function App() {
 	return (
 		<>
-			<div>
+			<div className="flex flex-col justify-center items-center min-h-screen">
 				<Suspense fallback={<Loading />}>
-					<blockquote>
-						{quote.quote}
-						<footer>Kanye West</footer>
-					</blockquote>
+					<div className="flex flex-col gap-6 text-zinc-800 max-w-sm rounded-lg p-6 bg-zinc-50 w-fit">
+						<blockquote className="italic">
+							“{quote.then((quote) => quote.quote)}”
+						</blockquote>
+						<div className="flex gap-2 items-center w-fit">
+							<img
+								src={kanyeImage}
+								className="size-5 object-cover rounded-full"
+								alt=""
+							/>
+							<span>Kanye West</span>
+						</div>
+					</div>
 				</Suspense>
 			</div>
 		</>
