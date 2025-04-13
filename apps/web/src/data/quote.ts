806aab05fa03ed6223d50aa7b020902e8de41e3f
Samuel <smlxdesign@duck.com>
2025-04-13 02:43:15 +0200

    fix small bug

--- apps/web/src/data/quote.ts
+++ apps/web/src/data/quote.ts
@@ -3,7 +3,7 @@ import { Quote } from '../schema/quote';
 async function loadQuote() {
 	return await fetch('https://api.kanye.rest/text')
 		.then((res) => {
-			return res.json();
+			return res.text();
 		})
 		.then((json) => {
 			return Quote.parse(json);
